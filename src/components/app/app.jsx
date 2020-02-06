f87c270496cf320c6db2e33ee5a1a773dcd271ea
Marina <snt.evas@gmail.com>
2020-02-06 19:51:20 +0100

    first components - fix "return"

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,9 +4,7 @@ import Main from "../main/main.jsx";
 // eslint-disable-next-line react/prop-types
 const App = ({film}) => {
 
-  return <React.Fragment>
-    return <Main film={film}/>;
-  </React.Fragment>;
+  return <Main film={film}/>;
 };
 
 
